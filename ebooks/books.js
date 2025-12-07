f4b43eab8d5c91fa3360869707fbadda383c93ce
hauv.fun <bhauve2012@gmail.com>
2025-12-07 13:24:52 -0500

    Update books.js

--- ebooks/books.js
+++ ebooks/books.js
@@ -1,7 +1,7 @@
 const books = [
   {
-    title: "My First Book",
-    pdf: "book1.pdf",
-    icon: "book1.png"
+    title: "Sword Art Online - Volume 1",
+    pdf: "SwordArtOnline-Volume1.pdf",
+    icon: "SAO-V1-Icon.png"
   }
 ];
