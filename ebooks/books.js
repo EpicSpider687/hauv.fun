17d05424055d747fb046a07b74b14de55008bfd9
hauv.fun <bhauve2012@gmail.com>
2025-12-08 19:17:12 -0500

    Update books.js

--- ebooks/books.js
+++ ebooks/books.js
@@ -4,4 +4,9 @@ const books = [
     pdf: "books/SwordArtOnline-Volume1/pdf/SwordArtOnline-Volume1.pdf",  // Correct path
     icon: "icons/SAO-V1-Icon.png"  // Correct path
   }
+  {
+    title: "Sword Art Online - Volume 2",
+    pdf: "books/SwordArtOnline-Volume2/pdf/SwordArtOnline-Volume2.pdf",  // Correct path
+    icon: "icons/SAO-V2-Icon.png"  // Correct path
+  }
 ];
